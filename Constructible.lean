7f792fc67adc5faa554ee6748b6d1641fba9af6c
Martin Bishop <bishopmje@gmail.com>
2025-06-24 15:13:49 -0400

    update

--- Constructible.lean
+++ Constructible.lean
@@ -1,3 +1,2 @@
--- This module serves as the root of the `Constructible` library.
--- Import modules here that should be built as part of the library.
-import Constructible.Basic
+import Constructible.Main
+import Constructible.alphadegree
