226703fa37585da80284b1b4f22e5b962ad6293c
fbarroero <fbarroero@gmail.com>
2025-06-24 11:05:19 -0400

    Approach with induction

--- def.lean
+++ def.lean
@@ -8,12 +8,49 @@ inductive IsConstructible : ℂ → Prop
   | inv (α : ℂ) : IsConstructible α → IsConstructible α⁻¹
   | rad (α : ℂ) : IsConstructible (α ^ 2) → IsConstructible α
 
+lemma IsConstructible.induction (P : ℂ → Prop) {α : ℂ} (hα : IsConstructible α)
+    (base : ∀ α : ℚ, P (algebraMap ℚ ℂ α))
+    (add : ∀ α β : ℂ, P α → P β → P (α + β))
+    (neg : ∀ α : ℂ, P α → P (-α))
+    (mul : ∀ α β : ℂ, P α → P β → P (α * β))
+    (inv : ∀ α : ℂ, P α → P α⁻¹)
+    (rad : ∀ α : ℂ, P (α ^ 2) → P α) :
+    P α := by
+  sorry
+
+lemma rank_eq_pow_two_of_isConstructible {x : ℂ} (h : IsConstructible x) :
+    ∃ n, x ≠ 0 → Module.finrank ℚ (Submodule.span ℚ {x}) = 2 ^ n := by
+  apply h.induction
+  · intro x
+    use 0
+    intro hx
+    simpa using finrank_span_singleton hx
+  sorry
+  sorry
+  sorry
+  sorry
+  sorry
+
+lemma minpoly_degree_eq_pow_two_of_isConstructible {x : ℂ} (h : IsConstructible x) :
+    ∃ n, x ≠ 0 → (minpoly ℚ x).natDegree = 2 ^ n := by
+  apply h.induction
+  · intro x
+    use 0
+    intro hx
+    exact minpoly.natDegree_eq_one_iff.mpr <| RingHom.mem_range_self (algebraMap ℚ ℂ) x
+  sorry
+  sorry
+  sorry
+  sorry
+  sorry
 
 lemma isConstructible_iff (x : ℂ) : IsConstructible x ↔
     ∃ (n : ℕ), ∃ f : Fin (n+1) → Subfield ℂ,
       ∀ i, ∃ (h : f i ≤ f (i+1)), x ∈ f (Fin.last n) ∧
       letI : Module (f i) (f (i+1)) := (Subfield.inclusion h).toAlgebra.toModule
       Module.finrank (f i) (f (i+1)) = 2 := by
+  let L := Submodule.span ℚ {x}
+
   sorry
 
 notation "α" => (2 : ℝ)^((1 : ℝ)/3)
