b6e5f20df108eec66ef435406df5d14c0f423ab1
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-06-26 18:07:05 -0400

    add this

--- Constructible.lean
+++ Constructible.lean
@@ -1,3 +1,7 @@
-import Constructible.Main
-import Constructible.alphadegree
 import Constructible.Lemmas
+import Constructible.Main
+import Constructible.QuadraticTower
+import Constructible.alphadegree
+import Constructible.main''
+import Constructible.thetadegree
+import Constructible.«marty main»
